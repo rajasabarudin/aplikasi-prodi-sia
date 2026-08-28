d5f0d5cf6736545f9c4cbed18c54f3d5fee3fc66
rajasabarudin <rajasabarudin@users.noreply.github.com>
2026-08-28 14:16:10 +0700

    chore: remove junk test files that block ftp deploy

--- test_count_hibah.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO('sqlite:database/database.sqlite');
-$countHibah = $db->query("SELECT COUNT(*) FROM hibah_penelitians")->fetchColumn();
-echo "Hibah Penelitian: " . $countHibah . "\n";
--- test_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new PDO('sqlite:database/database.sqlite');
-$result = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
-foreach ($result as $row) {
-    if (strpos($row['name'], 'penelitian') !== false || strpos($row['name'], 'pkm') !== false || strpos($row['name'], 'jurnal') !== false) {
-        echo $row['name'] . "\n";
-    }
-}
